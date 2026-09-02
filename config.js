cfe6abcc9c3bcb0a3b330be303d54a713559547a
OmniOx750 <mek03@mekics.com>
2026-09-02 10:27:39 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Google Apps Script 웹앱 배포 URL을 아래 따옴표 안에 붙여넣으세요.
 // 비워두면 내장된 초기 데이터로 화면을 확인할 수 있습니다.
-window.APP_CONFIG = {
+window.APP_CONFIG = {AKfycbwFI7yzi5yfzQiJFmoPChMsvPjididcCElPYOEnfiOVNCvA0EoSTRzl9HleorxsJxeU_g
   API_URL: ""
 };
